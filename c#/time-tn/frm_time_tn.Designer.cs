15f0319b1568f89023f92c50a38166bfb4886a25
Walid Mahfoudh <19792122+wmahfoudh@users.noreply.github.com>
2019-02-06 13:45:16 +0300

    changed background color

--- c#/time-tn/frm_time_tn.Designer.cs
+++ c#/time-tn/frm_time_tn.Designer.cs
@@ -62,7 +62,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.BackColor = System.Drawing.Color.Gold;
             this.ClientSize = new System.Drawing.Size(298, 56);
             this.ControlBox = false;
             this.Controls.Add(this.lb_time_tn);
